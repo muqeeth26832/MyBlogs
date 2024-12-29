b20995644de173a54dde7f5f4eb31bd0d98c5237
muqeeth26832 <mdabdulmuqeeth26832@gmail.com>
2024-12-29 20:49:57 +0530

    (feat): set layouts ,add global css, base components

---
+++ src/data/navData.ts
@@ -0,0 +1,10 @@
+interface navDataInterface {
+  name: string;
+  path: string;
+}
+const navData: navDataInterface[] = [
+  { name: "About", path: "/about/" },
+  { name: "Blog", path: "/blog/" },
+];
+
+export default navData;
